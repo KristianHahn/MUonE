a06d3dda092da8e3db856089d03abcc91f80925e
Kristian Hahn <khahn@cern.ch>
2021-09-26 11:38:34 -0500

    add sequence check

--- decode.c
+++ decode.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <sys/types.h>
 
@@ -38,6 +39,9 @@ main(int argc, char** argv)
   char buf[8]; //64b
   char * bufptr = &(buf[0]);
 
+  bool firsttime=true;
+  unsigned long seq_prev;
+  int bad_seq_cnt = 0;
   while ( !(feof(f)) ) {
 
     fread(buf,1,8,f); // 8x1B = 64b
@@ -45,6 +49,7 @@ main(int argc, char** argv)
     unsigned code = (unsigned)(buf[7])&0xff;
 
     if( code == 0xaa ) {
+
       printf("%02x%02x %02x%02x %02x%02x %02x%02x\n",
     	   0xff&buf[7], 0xff&buf[6],
     	   0xff&buf[5], 0xff&buf[4],
@@ -65,6 +70,17 @@ main(int argc, char** argv)
       printf( "\trun#     : 0x%3x\n",      run);      
       printf( "\tseq#     : 0x%09llx\n",   seq);      	
 
+
+      // check sequence
+      if( !firsttime ) { 
+	if( seq != seq_prev+1 ) { 
+	  printf( "ERROR :: bad sequence : 0x%09llx vs expected 0x%09llx\n", 
+		  seq, seq_prev+1);
+	  bad_seq_cnt++;
+	}
+      }
+      seq_prev = seq;
+
       printf("\n   -- data start -- \n");
       for( int wrdrd=0; wrdrd<pktlen; wrdrd++ ) {  
 	fread(buf,1,8,f);
@@ -77,8 +93,11 @@ main(int argc, char** argv)
       }
       printf("   -- data end -- \n\n\n");
 
+      firsttime = false;
       // now return to main read loop, look again for the hdr code ...
     }
   } //while !eof
+
+  printf ("\ndone\nsequence errors : %d\n\n", bad_seq_cnt);
 
 }
